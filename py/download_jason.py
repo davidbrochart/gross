cacc4e7b47dff954cb7e6133dde2019602953aa0
David Brochart <david.brochart@gmail.com>
2016-01-18 07:44:13 +0100

    Added FTP exception handling

--- py/download_jason.py
+++ py/download_jason.py
@@ -14,22 +14,28 @@ ftp.dir(lines.append)
 dirnames = [line.split()[-1] for line in lines]
 dirnames = [dirname for dirname in dirnames if dirname.startswith('cycle') and int(dirname[5:]) >= 246]
 
-# copy files from every FTP directory to destination directory:
-for dirname in dirnames:
-    ftp.cwd(dirname)
-    lines = []
-    ftp.dir(lines.append)
-    filenames = [line.split()[-1] for line in lines]
-    for filename in filenames:
-        if filename.endswith('.nc'):
-            destname = dest + '/' + filename
-            if not os.path.exists(destname):
-                print('Downloading to ' + destname)
-                destfile = open(destname, 'wb')
-                ftp.retrbinary("RETR " + filename, destfile.write)
-                destfile.close()
-            else:
-                print('File ' + destname + ' already downloaded')
-    ftp.cwd('..')
+done = False
+while not done:
+    try:
+        # copy files from every FTP directory to destination directory:
+        for dirname in dirnames:
+            ftp.cwd(dirname)
+            lines = []
+            ftp.dir(lines.append)
+            filenames = [line.split()[-1] for line in lines]
+            for filename in filenames:
+                if filename.endswith('.nc'):
+                    destname = dest + '/' + filename
+                    if not os.path.exists(destname):
+                        print('Downloading to ' + destname)
+                        destfile = open(destname, 'wb')
+                        ftp.retrbinary("RETR " + filename, destfile.write)
+                        destfile.close()
+                    else:
+                        print('File ' + destname + ' already downloaded')
+            ftp.cwd('..')
+        done = True
+    except:
+        os.system('rm -f ' + destname)
 
 ftp.quit()
